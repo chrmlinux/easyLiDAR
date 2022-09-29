5dccbf149be2a2d3a4fc62f0cb6fa04210e63de7
chrmlinux03 <96155486+chrmlinux@users.noreply.github.com>
2022-09-29 21:08:39 +0900

    mpu/fastled add

--- examples/1st/1st.ino
+++ examples/1st/1st.ino
@@ -1,19 +1,29 @@
 #include "easyLiDAR.hpp"
+#include "FastLed.h"
 
 static easyLiDAR lidar;
 
 #define I2C_SDA (25)
 #define I2C_SCL (21)
-#define I2C_FREQ (400 * 1000)
+#define I2C_FREQ (1000 * 1000)
 
 void setup(void) {
   Serial.begin( 115200 ); while(!Serial); delay( 100 );
   Wire.begin(I2C_SDA, I2C_SCL);
   Wire.setClock(I2C_FREQ);
   lidar.begin();
+  setupLeds();
 }
 
 void loop(void) {
+  static int stat = 0;
+  static uint32_t tm = millis();
+
   lidar.update();
-  lidar.dump(0, 8);
+  lidar.dump(0, 16);
+  if ((millis() - tm) > 1000) {
+    tm = millis();
+    leds[12] = (stat * 255) << 16; FastLED.show();
+    stat = !stat;
+  }
 }
